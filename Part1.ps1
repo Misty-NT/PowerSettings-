de263a412df938f49fc3ae599680fafff6bdaa80
Misty - NT <166186521+Misty-NT@users.noreply.github.com>
2024-10-29 15:42:10 -0700

    Update Part1.ps1

--- Part1.ps1
+++ Part1.ps1
@@ -12,10 +12,6 @@ $powerScheme = powercfg -query
 $powerScheme | Out-File -FilePath $powerSettingsFile
 
 # Set power settings to "never off" for both plugged-in and battery modes
-powercfg -change -monitor-timeout-ac 0   # Never turn off display when plugged in
-powercfg -change -monitor-timeout-dc 0   # Never turn off display on battery
-powercfg -change -disk-timeout-ac 0      # Never turn off hard disk when plugged in
-powercfg -change -disk-timeout-dc 0      # Never turn off hard disk on battery
 powercfg -change -standby-timeout-ac 0   # Never sleep when plugged in
 powercfg -change -standby-timeout-dc 0   # Never sleep on battery
 powercfg -change -hibernate-timeout-ac 0 # Never hibernate when plugged in
